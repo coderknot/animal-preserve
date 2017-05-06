9a28964dcbebd8a0a093b48f36b05498b56b2582
Chris Carr <coder.knot@gmail.com>
2017-05-05 19:24:06 -0700

    add button to show edit form for selected animal

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,15 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
 
 @Component({
   selector: 'animal-list',
   template: `
   <li *ngFor="let animal of animalList">
-    {{animal.name}}
+    {{animal.name}} <button class="btn btn-default" (click)="editButtonClicked(animal)">Update Animal Record</button>
   </li>
   `
 })
 
 export class AnimalListComponent {
   @Input() animalList: Animal[];
+  @Output() editButtonClickSender = new EventEmitter();
+
+  editButtonClicked(animalToEdit: Animal) {
+    this.editButtonClickSender.emit(animalToEdit);
+  }
 }
